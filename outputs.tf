136f9a4f2eba11b8a57976e41669128f8776209b
broadbounds <84296908+broadbounds@users.noreply.github.com>
2021-08-02 15:17:19 -0700

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,55 @@
-output "bastion_host_1_public_ip" {  
-  value = "${aws_eip.bastion_elastic_ip_1.public_ip}"
+#
+# Outputs
+#
+
+locals {
+  config_map_aws_auth = <<CONFIGMAPAWSAUTH
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: aws-auth
+  namespace: kube-system
+data:
+  mapRoles: |
+    - rolearn: ${aws_iam_role.demo-node.arn}
+      username: system:node:{{EC2PrivateDNSName}}
+      groups:
+        - system:bootstrappers
+        - system:nodes
+CONFIGMAPAWSAUTH
+
+  kubeconfig = <<KUBECONFIG
+apiVersion: v1
+clusters:
+- cluster:
+    server: ${aws_eks_cluster.demo.endpoint}
+    certificate-authority-data: ${aws_eks_cluster.demo.certificate_authority[0].data}
+  name: kubernetes
+contexts:
+- context:
+    cluster: kubernetes
+    user: aws
+  name: aws
+current-context: aws
+kind: Config
+preferences: {}
+users:
+- name: aws
+  user:
+    exec:
+      apiVersion: client.authentication.k8s.io/v1alpha1
+      command: aws-iam-authenticator
+      args:
+        - "token"
+        - "-i"
+        - "${var.cluster-name}"
+KUBECONFIG
 }
 
-output "bastion_host_1_private_ip" {  
-  value = "${aws_instance.bastion_host_1.private_ip}"
+output "config_map_aws_auth" {
+  value = local.config_map_aws_auth
 }
 
-output "bastion_host_2_public_ip" {  
-  value = "${aws_eip.bastion_elastic_ip_2.public_ip}"
-}
-
-output "bastion_host_2_private_ip" {  
-  value = "${aws_instance.bastion_host_2.private_ip}"
+output "kubeconfig" {
+  value = local.kubeconfig
 }

46fbbaf5dcab451a9baf8944705bdb1129865990
broadbounds <84296908+broadbounds@users.noreply.github.com>
2021-08-02 20:45:36 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,6 +18,26 @@ data "aws_availability_zones" "available" {}
 provider "http" {}
 
 
+#
+# Workstation External IP
+#
+# This configuration is not required and is
+# only provided as an example to easily fetch
+# the external IP of your local workstation to
+# configure inbound EC2 Security Group access
+# to the Kubernetes cluster.
+#
+
+data "http" "workstation-external-ip" {
+  url = "http://ipv4.icanhazip.com"
+}
+
+# Override with variable or hardcoded value if necessary
+locals {
+  workstation-external-cidr = "${chomp(data.http.workstation-external-ip.body)}/32"
+}
+
+
 
 #
 # VPC Resources
